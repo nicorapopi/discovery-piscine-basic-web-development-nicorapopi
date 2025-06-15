ccf18e0b37b9dcbb95392f9c23ba0f92aed037ad
nicorapopi <126184406+nicorapopi@users.noreply.github.com>
2025-06-15 13:47:06 +0000

    เช็คอาร์กิวเมนต์เป็น 0 หรือไม่ ถ้าใช่ แสดงข้อความและออกจากโปรแกรม

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,15 @@
+if [ $# -eq 0 ]; then
+  echo "No arguments supplied"
+  exit 0
+fi
+
+count=0
+for arg in "$@"; do
+  echo "$arg"
+  count=$((count + 1))
+  if [ $count -ge 3 ]; then
+    break
+  fi
+done
+
+#bash argv.sh [ ]
